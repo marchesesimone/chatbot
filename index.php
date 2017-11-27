f9c9e235383223369a5999938abd1490174779cf
Marchese Simone <smarchese@wellnet.it>
2017-11-27 12:15:11 +0100

    add msg reply hello

--- index.php
+++ index.php
@@ -27,9 +27,5 @@ $botman->hears('Ciao', function (BotMan $bot) {
 });
 
 
-$bot->reply(ButtonTemplate::create('Do you want to know more about BotMan?')
-  ->addButton(ElementButton::create('Tell me more')->type('postback')->payload('tellmemore'))
-  ->addButton(ElementButton::create('Show me the docs')->url('http://botman.io/'))
-);
 
 $botman->listen();
