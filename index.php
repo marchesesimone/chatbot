b06db8b8579093e0565105c57fc53def1b1384ff
Marchese Simone <smarchese@wellnet.it>
2017-11-27 10:58:26 +0100

    edit config paramaters

--- index.php
+++ index.php
@@ -6,11 +6,8 @@ use BotMan\BotMan\BotManFactory;
 
 $config = [
   // Your driver-specific configuration
-  'facebook' => [
-    'token' => 'EAAHCF500w5sBAOjpYTJOQPYlEF58oRae1gdNVPRLxWY2g4Iya8axaYWRTp0OGJDnujRuzRyTH2ZAAzRSZBZCb5gmpXOz2RDWPugZAYsiH5mZC2IiZBg1ESMzgqRxk3ZBTWTXNhUG55DeZAe0eo4M850KEIkgieIZC78JnYFFOQSHRNAZDZD',
-    'app_secret' => '8dbc5620f7bb9c4e3c6ccb7bd51f30b8',
-    'verification'=>'tutorialbotfacebook-verify',
-  ]
+  'facebook_token' => 'EAAHCF500w5sBAOjpYTJOQPYlEF58oRae1gdNVPRLxWY2g4Iya8axaYWRTp0OGJDnujRuzRyTH2ZAAzRSZBZCb5gmpXOz2RDWPugZAYsiH5mZC2IiZBg1ESMzgqRxk3ZBTWTXNhUG55DeZAe0eo4M850KEIkgieIZC78JnYFFOQSHRNAZDZD',
+  'facebook_app_secret' => '8dbc5620f7bb9c4e3c6ccb7bd51f30b8',
 ];
 
 // create an instance
