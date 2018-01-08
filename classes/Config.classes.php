a0195928724eeb9af5915b0f072ca534e81071c5
Marchese Simone <smarchese@wellnet.it>
2018-01-08 17:23:35 +0100

    rename config class

--- classes/Config.classes.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Config {
-  static ALGOLIA_APP_ID = '13PFPB21EY';
-  static ALGOLIA_SECRET = '64e2cd0f6f712572b51c9d9d212a7520';
-  static ALGOLIA_INDEX = '';
-}
-
-echo "Class Config loaded.\n";
