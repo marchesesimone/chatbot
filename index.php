fd4ff0f3599859f44750f4bc844716da8baca8a6
Marchese Simone <smarchese@wellnet.it>
2018-01-08 17:47:21 +0100

    add readme.md and set config facebook in class

--- index.php
+++ index.php
@@ -9,21 +9,8 @@ use Mpociot\BotMan\Facebook\ElementButton;
 use Mpociot\BotMan\Facebook\ButtonTemplate;
 
 $config = [
-  'hipchat_urls' => [
-    'YOUR-INTEGRATION-URL-1',
-    'YOUR-INTEGRATION-URL-2',
-  ],
-  'nexmo_key' => 'YOUR-NEXMO-APP-KEY',
-  'nexmo_secret' => 'YOUR-NEXMO-APP-SECRET',
-  'microsoft_bot_handle' => 'YOUR-MICROSOFT-BOT-HANDLE',
-  'microsoft_app_id' => 'YOUR-MICROSOFT-APP-ID',
-  'microsoft_app_key' => 'YOUR-MICROSOFT-APP-KEY',
-  'slack_token' => 'YOUR-SLACK-TOKEN-HERE',
-  'telegram_token' => 'YOUR-TELEGRAM-TOKEN-HERE',
-  'facebook_token' => 'EAAHCF500w5sBAOjpYTJOQPYlEF58oRae1gdNVPRLxWY2g4Iya8axaYWRTp0OGJDnujRuzRyTH2ZAAzRSZBZCb5gmpXOz2RDWPugZAYsiH5mZC2IiZBg1ESMzgqRxk3ZBTWTXNhUG55DeZAe0eo4M850KEIkgieIZC78JnYFFOQSHRNAZDZD',
+  'facebook_token' => Config::FACEBOOK_TOKEN,
   'facebook_app_secret' => Config::FACEBOOK_APP_SECRET,
-  'wechat_app_id' => 'YOUR-WECHAT-APP-ID',
-  'wechat_app_key' => 'YOUR-WECHAT-APP-KEY',
 ];
 
 
