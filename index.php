e42457d8f687f95cb84a70b8352d280f868f18a7
Marchese Simone <simone.marchese@wellnet.it>
2018-03-09 09:57:37 +0100

    remove sleep

--- index.php
+++ index.php
@@ -42,7 +42,7 @@ $botman->hears('Hello', function(BotMan $bot) {
 });
 
 // NPL
-$botman->hears('(.*)', function(BotMan $bot) {
+$botman->hears('sorry(.*)', function(BotMan $bot) {
   $extras = $bot->getMessage()->getExtras();
   $apiReply = $extras['apiReply'];
   $bot->reply($apiReply);
