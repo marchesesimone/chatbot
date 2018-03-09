d7abfabefa153753a3228f5a9206c5ad0b9a7410
Marchese Simone <simone.marchese@wellnet.it>
2018-03-09 18:10:44 +0100

    welcome_payload

--- index.php
+++ index.php
@@ -36,6 +36,13 @@ $botman->hears('Hi, WELCOME_PAYLOAD', function (BotMan $bot) {
   );
 });
 
+$botman->hears('WELCOME_PAYLOAD', function (BotMan $bot) {
+  $bot->reply(ButtonTemplate::create('Welcome to Wellnet! How can I help you?')
+    ->addButton(ElementButton::create('Where we are')->url('https://www.google.it/maps/place/Wellnet+S.r.l./@45.467766,9.173752,17z/data=!3m1!4b1!4m5!3m4!1s0x4786c151cfb6560f:0x529a891fd0d58a8c!8m2!3d45.4677623!4d9.175946'))
+    ->addButton(ElementButton::create('Contact')->url('https://www.wellnet.it/contatti'))
+    ->addButton(ElementButton::create('Want a quote?')->url('https://www.wellnet.it/contatti'))
+  );
+});
 $botman->hears('Hello', function(BotMan $bot) {
   $bot->startConversation(new Facebook());
 });
