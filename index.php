cee9ab70323f11881ff61a21de87838fcf9fe96a
Marchese Simone <simone.marchese@wellnet.it>
2018-04-03 16:23:08 +0200

    unsubscribe user in DB

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/classes/basic.php';
+require __DIR__ . '/classes/DB.class.php';
 
 use Mpociot\BotMan\BotManFactory;
 use Mpociot\BotMan\BotMan;
@@ -8,7 +9,6 @@ use Mpociot\BotMan\Facebook\ElementButton;
 use Mpociot\BotMan\Facebook\ButtonTemplate;
 use Mpociot\BotMan\Cache\DoctrineCache;
 use Doctrine\Common\Cache\FilesystemCache;
-use \DBApp\DB;
 
 $config = [
   'facebook_token' => 'EAAHCF500w5sBAOjpYTJOQPYlEF58oRae1gdNVPRLxWY2g4Iya8axaYWRTp0OGJDnujRuzRyTH2ZAAzRSZBZCb5gmpXOz2RDWPugZAYsiH5mZC2IiZBg1ESMzgqRxk3ZBTWTXNhUG55DeZAe0eo4M850KEIkgieIZC78JnYFFOQSHRNAZDZD',
@@ -46,9 +46,8 @@ $botman->fallback(function(BotMan $bot) {
   $bot->reply('Here is a list of commands I understand: Hi, Hello and Contact');
 });
 
-$botman->hears('unsubscribe_yes', function(Botman $bot) {
+$botman->hears('unsubscribe_yes', function(Botman $bot, DBApp\DB $con) {
 
-  $con = new DB();
   $bot->reply('Start connection DB');
   $bot_id = $bot->getUser()->getId();
   $del_user = $con->delete('user', 'botman_id', $bot_id);
