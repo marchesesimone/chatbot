4b4796cb1048e08c818915621b1bd8a3ce82ffab
Marchese Simone <simone.marchese@wellnet.it>
2018-04-03 10:11:45 +0200

    test connection

--- classes/Facebook.class.php
+++ classes/Facebook.class.php
@@ -32,8 +32,7 @@ class Facebook extends Conversation {
   protected $con;
 
   public function __construct() {
-    $db = new \DBApp\DB();
-    $this->con = $db::getInstance();
+    $this->con = new \DBApp\DB();
   }
 
   /**
