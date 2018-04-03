90b772c617c33d2db9422fe7ca722393a735ee8e
Marchese Simone <simone.marchese@wellnet.it>
2018-04-03 16:36:39 +0200

    add required Facebook.class in index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/classes/Facebook.class.php';
 require __DIR__ . '/classes/Config.class.php';
 require __DIR__ . '/classes/DB.class.php';
 
