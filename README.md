fb5ba45ac3a422b1e5b250895710066e021cec19
Simone Marchese <simone.marchese@wellnet.it>
2019-08-23 10:24:17 +0200

    Remove file Config.class.php and ccreate dist file

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
-## Config botman
+## Chatbot Tutorial
 
+Questo codice è stato utilizzato per scrivere il seguente articolo
+https://www.wellnet.it/blog/tutorial/come-creare-un-chatbot-facebook-messenger-parte-1
+
+## Requisiti
+1. Avere una pagina Facebook ed essere amministratori
+2. Utilizzare php 7
+
+## Installazione
+Copiare il file `classes/Config.class.php.dist` in `classes/Config.class.php` e valorizzare le costanti di Facebook
+Dalla root del progetto lanciare `composer install`
+
+## Config botman
 ```
 $config = [
   'hipchat_urls' => [
--- index.php
+++ index.php
@@ -11,10 +11,11 @@ use Mpociot\BotMan\Facebook\ButtonTemplate;
 use Mpociot\BotMan\Cache\DoctrineCache;
 use Doctrine\Common\Cache\FilesystemCache;
 use \DBApp\DB;
+use \ConfigApp\Config;
 
 $config = [
-  'facebook_token' => 'EAAHCF500w5sBAOjpYTJOQPYlEF58oRae1gdNVPRLxWY2g4Iya8axaYWRTp0OGJDnujRuzRyTH2ZAAzRSZBZCb5gmpXOz2RDWPugZAYsiH5mZC2IiZBg1ESMzgqRxk3ZBTWTXNhUG55DeZAe0eo4M850KEIkgieIZC78JnYFFOQSHRNAZDZD',
-  'facebook_app_secret' => '8dbc5620f7bb9c4e3c6ccb7bd51f30b8',
+  'facebook_token' => Config::FACEBOOK_TOKEN,
+  'facebook_app_secret' => Config::FACEBOOK_APP_SECRET,
 ];
 
 // create an instance and settings cache for conversation system
